5a16aeb65e9496e445ee910718cdeb03caff4670
quyench <quyench@pateco.vn>
2017-05-10 15:54:23 +0700

    Upate

--- application/views/user/site/home.php
+++ application/views/user/site/home.php
@@ -33,8 +33,8 @@
 					<div class="box-menu">
 						<a href="<?=ROOT?>hai-san/<?=$value['slug']?>.html" title="<?=$value['name']?>">
 							<img src="<?=ROOT?>upload/cate/350/<?=$value['image']?>" alt="<?=$value['name']?>">
-							<div class="overlay"></div>
-							<span class="text"><?=$value['name']?></span>
+							<div class="overlay">asdasdasasdasdasdasdasd</div>
+							<span class="text"><?=$value['name']?>asdasda</span>
 						</a>
 					</div>
 				</div>
